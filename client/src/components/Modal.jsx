6d485f9f715f10f90d5561f16c338b98ef0a17a0
mcc1461 <mcoskuncelebi@gmail.com>
2025-01-12 23:08:16 +0300

    .

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -5,8 +5,7 @@ const Modal = () => {
   return (
     <>
       <button
-        className="bg-blue-200 text-black active:bg-blue-500 
-      font-bold px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
+        className="px-6 py-3 mb-1 mr-1 font-bold text-black bg-blue-200 rounded shadow outline-none active:bg-blue-500 hover:shadow-lg focus:outline-none"
         type="button"
         onClick={() => setShowModal(true)}
       >
@@ -14,50 +13,50 @@ const Modal = () => {
       </button>
       {showModal ? (
         <>
-          <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t ">
+          <div className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none">
+            <div className="relative w-auto max-w-3xl mx-auto my-6">
+              <div className="relative flex flex-col w-full bg-white border-0 rounded-lg shadow-lg outline-none focus:outline-none">
+                <div className="flex items-start justify-between p-5 border-b border-gray-300 border-solid rounded-t ">
                   <h3 className="text-3xl font=semibold">General Info</h3>
                   <button
-                    className="bg-transparent border-0 text-black float-right"
+                    className="float-right text-black bg-transparent border-0"
                     onClick={() => setShowModal(false)}
                   >
-                    <span className="text-black opacity-7 h-6 w-6 text-xl block bg-gray-400 py-0 rounded-full">
+                    <span className="block w-6 h-6 py-0 text-xl text-black bg-gray-400 rounded-full opacity-7">
                       x
                     </span>
                   </button>
                 </div>
-                <div className="relative p-6 flex-auto">
-                  <form className="bg-gray-200 shadow-md rounded px-8 pt-6 pb-8 w-full">
-                    <label className="block text-black text-sm font-bold mb-1">
+                <div className="relative flex-auto p-6">
+                  <form className="w-full px-8 pt-6 pb-8 bg-gray-200 rounded shadow-md">
+                    <label className="block mb-1 text-sm font-bold text-black">
                       First Name
                     </label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black" />
-                    <label className="block text-black text-sm font-bold mb-1">
+                    <input className="w-full px-1 py-2 text-black border rounded shadow appearance-none" />
+                    <label className="block mb-1 text-sm font-bold text-black">
                       Last Name
                     </label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black" />
-                    <label className="block text-black text-sm font-bold mb-1">
+                    <input className="w-full px-1 py-2 text-black border rounded shadow appearance-none" />
+                    <label className="block mb-1 text-sm font-bold text-black">
                       Address
                     </label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black" />
-                    <label className="block text-black text-sm font-bold mb-1">
+                    <input className="w-full px-1 py-2 text-black border rounded shadow appearance-none" />
+                    <label className="block mb-1 text-sm font-bold text-black">
                       City
                     </label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black" />
+                    <input className="w-full px-1 py-2 text-black border rounded shadow appearance-none" />
                   </form>
                 </div>
-                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                <div className="flex items-center justify-end p-6 border-t border-solid rounded-b border-blueGray-200">
                   <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1"
+                    className="px-6 py-2 mb-1 mr-1 text-sm font-bold text-red-500 uppercase outline-none background-transparent focus:outline-none"
                     type="button"
                     onClick={() => setShowModal(false)}
                   >
                     Close
                   </button>
                   <button
-                    className="text-white bg-yellow-500 active:bg-yellow-700 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
+                    className="px-6 py-3 mb-1 mr-1 text-sm font-bold text-white uppercase bg-yellow-500 rounded shadow outline-none active:bg-yellow-700 hover:shadow-lg focus:outline-none"
                     type="button"
                     onClick={() => setShowModal(false)}
                   >
