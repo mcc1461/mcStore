c1fd8d537bf93cebfb937e4d617ccba6af40e4ef
mcc1461 <mcoskuncelebi@gmail.com>
2025-03-16 21:09:19 +0300

    fix: CategoryUtils destination.

--- client/src/utils/CategoryList.jsx
+++ client/src/utils/CategoryList.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Dialog, Transition } from "@headlessui/react";
 import apiClient from "../services/apiClient";
-import { computeCategorySummary, parseNumber } from "../utils/categoryUtils";
+import { computeCategorySummary, parseNumber } from "../utils/CategoryUtils";
 import { formatCurrency } from "./..formatCurrency";
 
 // Define background colors for category cards.
